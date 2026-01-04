8a93c194d8d01bb552fd4c13bf72fec20bfe5d3a
Pavel Uvarov <spiderpoul@mail.ru>
2026-01-04 23:21:02 +0300

    Add Decap CMS preview templates

---
+++ pages/admin/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import Script from 'next/script';
+
+export default function AdminPage() {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Контент-редактор</title>
+      </Head>
+      <Script
+        src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"
+        strategy="beforeInteractive"
+      />
+      <Script src="/admin/preview.js" strategy="afterInteractive" />
+    </>
+  );
+}
